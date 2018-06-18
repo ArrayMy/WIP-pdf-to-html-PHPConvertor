23fd591d0fabab2d0201a042bdf26787618c5386
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2018-06-18 09:49:00 +0200

    Update convertor.php

--- App/convertor.php
+++ App/convertor.php
@@ -1,18 +1,38 @@
 <?php
 class PDF_Convertor{
   
+	
+	
  public function __construct(){
-  $this->open_and_load_config();
-  $this->check_file();
+  /*$this->check_file();*/
  }
- 
- public function open_and_load_config(){
-  $this->CONFIG_PDF_FILE = parse_ini_file("_DIR_ . \"/../../config.ini",true);
- }
- 
+	 
+	public function load_data_from_config(){
+	$this->Config = parse_ini_file("../../Config/config.ini",true);	
+	}
+	
+ public function upload_pdf_file(){
+	$this->load_data_from_config();
+	$DIR = $this->Config['PDF']['PDF_Upload_Files_Path'];
+	$UPLOAD_FILE = $DIR . basename($_FILES['PDF_FILE']['name']);
+	 if($_FILES['PDF_FILE']['size']<=$this->Config['PDF']['PDF_Max_Size']){
+		 if($_FILES['PDF_FILE']['type']=='application/pdf'){
+	       if(move_uploaded_file($_FILES['PDF_FILE']['tmp_name'],$UPLOAD_FILE)){
+		     echo "Succesfull im converting!";
+		   }else{
+		   }
+	 }else{
+		  echo "Upload only pdf files, not ".$_FILES['PDF_FILE']['type']." files.";
+		 }
+	 }else{
+		 echo "Max size is ".$this->Config['PDF']['PDF_Max_Size']." .";
+	 }
+	 
+}
+
  public function check_file(){
   $this->PDF = pdf_new();
-  if(pdf_open_pdi_document($this->PDF,'$this->CONFIG_PDF_FILE['PDF_INFO']['PDF_FILE_NAME']')===false){
+	 if(pdf_open_pdi_document($this->PDF,$_FILES['PDF_FILE']['name'])===false){
    print error;
    exit;
   }else{
